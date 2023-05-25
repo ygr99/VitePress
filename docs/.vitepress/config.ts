43ccf7da4aea94f74236b0def406b3ea2eaa34f8
ygr_99 <196156709@qq.com>
2023-05-25 17:12:19 +0800

    测试 自动部署

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ import { page } from "./const/links";
 export default defineConfig({
   base: "/VitePress/",
   title: "一个人丶99",
-  description: "VitePress 一个人丶99",
+  description: "VitePress 一个人丶99,搭好了，后续再想想拿来做什么",
   head: [['link', { rel: 'icon', href: '/picture/avatar.jpg' }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
